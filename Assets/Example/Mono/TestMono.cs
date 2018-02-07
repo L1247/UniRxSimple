b8be320303b10c4674e801a78ea788dd23cf9956
Star <starlove124760@gmail.com>
2018-02-08 02:56:59 +0800

    加入按鈕自定義常按事件

---
+++ Assets/Example/Mono/TestMono.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestMono : MonoBehaviour
+{
+    [SerializeField] private GameObject button;
+
+    private void Start()
+    {
+        
+        var trigger = button.AddComponent<ObservableLongPointerDownTrigger>();
+
+        trigger.OnLongPointerDownAsObservable().Subscribe(_ => Debug.Log("Click"));
+    }
+}
